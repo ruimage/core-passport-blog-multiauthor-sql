21b390ca8f44dec2ee205b04d0e023cefda23ed5
abakharev <abakharev@sharevault.com>
2022-05-24 16:02:29 +0300

    replace hbs with react

--- views/entries/EntriesList.jsx
+++ views/entries/EntriesList.jsx
@@ -9,7 +9,7 @@ module.exports = function EntriesList({ entries }) {
       <main role="main">
         <ul className="entries-list no-bullets no-padding">
           {entries.map((entry) => (
-            <li className="entry-item pad-b-4">
+            <li className="entry-item pad-b-4" key={entry.id}>
               <a href={`/entries/${entry.id}`} className="entry-title font-2 pad-b-1-4 c-white">{entry.title}</a>
               <span className="entry-date block font-3-4 c-lt-gray">Written on {entry.createdAt.toString()}</span>
               <p className="entry-stub">{entry.body}</p>
